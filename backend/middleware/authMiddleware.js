535d2ba90a47851d59247fef7f92bac5d25f1bd6
Aryam Gupta <aryamgupta4@gmail.comgit config --global user.name Aryamgupta--global to set the identity only in this respositorygit remote add origin github.com/Aryamgupta/2000910100033.git>
2023-10-09 19:45:03 +0530

    first commit

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../Models/usersModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token = req.headers.token;
+  // console.log(token);
+  if (token) {
+    try {
+
+  //     //decodes token id
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = { protect };
